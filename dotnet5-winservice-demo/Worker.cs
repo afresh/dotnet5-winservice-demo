c45d235205298bc93c2f3d8a63ca8a75de04f319
Afresh <danding_ge@qq.com>
2020-12-31 17:29:30 +0800

    rewrite worker.cs

--- dotnet5-winservice-demo/Worker.cs
+++ dotnet5-winservice-demo/Worker.cs
@@ -21,15 +21,33 @@ namespace dotnet5_winservice_demo
             _configuration = configuration; //依赖注入IConfiguration
         }
 
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        public override async Task StartAsync(CancellationToken cancellationToken)
         {
+            _logger.LogInformation("Worker starting at: {time}", DateTimeOffset.Now);
+
             HostedServiceOptions = _configuration.GetSection(HostedServiceOptions.HostedService).Get<HostedServiceOptions>(); //绑定并返回指定的类型
 
-            while (!stoppingToken.IsCancellationRequested)
+            await base.StopAsync(cancellationToken);
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            await Task.Run(async () =>
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(HostedServiceOptions.WorkerInterval, stoppingToken); //设置托管任务轮询时间
-            }
+                //如果服务被停止，那么下面的IsCancellationRequested会返回true，我们就应该结束循环
+                while (!stoppingToken.IsCancellationRequested)
+                {
+                    _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                    await Task.Delay(HostedServiceOptions.WorkerInterval, stoppingToken); //设置托管任务轮询时间
+                }
+            }, stoppingToken);
+        }
+
+        public override async Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Worker stopping at: {time}", DateTimeOffset.Now);
+
+            await base.StopAsync(cancellationToken);
         }
     }
 }
